5194ec4bed0ccca06e3e8187bc29514226504233
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-04-28 01:19:47 +0900

    [Silver IV] Title: 한수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,14 @@
+N = int(input())
+count = 0
+
+for i in range(1, N + 1):
+    if i < 100:
+        count += 1
+    elif i < 1000:
+        a = i // 100      
+        b = (i // 10) % 10   
+        c = i % 10  
+        if a - b == b - c:
+            count += 1
+
+print(count)
