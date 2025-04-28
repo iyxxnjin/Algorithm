3ed9dee3ddaebd0cafe3ae621b90327bb32bc4cf
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-04-28 21:07:20 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -0,0 +1,18 @@
+def solution(rank, attendance):
+    answer = 0
+    candidates = []
+    
+    for i in range(len(attendance)):
+        if attendance[i]:
+            candidates.append((rank[i], i))
+                            
+    candidates.sort()
+    
+    a = candidates[0][1]
+    b = candidates[1][1]
+    c = candidates[2][1]
+    
+    answer = 10000 * a + 100 * b + c
+    return answer
+                            
+            
