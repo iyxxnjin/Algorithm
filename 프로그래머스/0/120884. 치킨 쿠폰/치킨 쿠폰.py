113a1369d9cc68c19e36dcee4399e02d2b56f11f
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-05-21 23:55:34 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,9 @@
+def solution(chicken):
+    answer = 0
+    
+    while chicken >= 10:
+        div, mod = divmod(chicken, 10)
+        answer += div
+        chicken = div + mod
+        
+    return answer
