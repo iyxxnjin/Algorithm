1b2be620e854453d5ccf21189bcc55c6e22c7626
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-05-27 12:37:45 +0900

    [Silver V] Title: 영화감독 숌, Time: 524 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+count = 0
+num = 666
+
+while True:
+    if '666' in str(num):
+        count += 1
+        if count == N:
+            print(num)
+            break
+    num += 1
