9ae1b5a2c6ab06309da53e2d89fc392aacadaefe
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-05-31 16:08:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,34 +1,18 @@
 def solution(lottos, win_nums):
     count = 0
-    zero = 0
+    low = 0
     result = []
     
-    for i in lottos:
-        if i == 0:
-            zero += 1
-        for j in win_nums:
-            if i == j:
-                count += 1
-                break
-
-    low = count
-    high = count + zero
+    rank_dict = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6}
     
-    def get_rank(n):
-        if n == 6:
-            return 1
-        elif n == 5:
-            return 2
-        elif n == 4:
-            return 3
-        elif n == 3:
-            return 4
-        elif n == 2:
-            return 5
-        else:
-            return 6
+    for i in lottos:
+        if i in win_nums:
+            low += 1
+            
+    zero = lottos.count(0)
+    high = low + zero
 
-    result.append(get_rank(high))
-    result.append(get_rank(low))
+    result.append(rank_dict[high])
+    result.append(rank_dict[low])
     
     return result
