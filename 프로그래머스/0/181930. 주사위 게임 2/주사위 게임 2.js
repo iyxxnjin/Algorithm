6a607d0912dc9f08b5ed40e1f2e15b5d566bf55d
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-06-21 12:17:27 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,17 +1,17 @@
 function solution(a, b, c) {
     let result = 0
-    let 합 = a+b+c
-    let 제곱 = a*a+b*b+c*c
-    let 세제곱 = a*a*a+b*b*b+c*c*c
+    let 합 = a + b + c
+    let 제곱 = a*a + b*b + c*c
+    let 세제곱 = a**3 + b**3 + c**3
     
-    if (a == b && b == c){
-        result = 합*제곱*세제곱
+    if (a === b && b === c){
+        result = 합 * 제곱 * 세제곱
     }
-    else if (a != b && a != c && b != c) {
-        result = 합
+    else if (a === b || b === c || c === a) {
+        result = 합 * 제곱
     }
     else {
-        result = 합*제곱
+        result = 합
     }
     
     return result
