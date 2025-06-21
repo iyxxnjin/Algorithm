b8cb3fef5b4a7462792636aeca43bccc31106aa9
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-06-22 02:36:00 +0900

    [level 0] Title: 배열 만들기 2, Time: 119.75 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -2,10 +2,9 @@ function solution(l, r) {
     let result = [];
     
     for (let i=l; i<=r; i++) {
-        let string_i = i.toString();
         
-        if ([...string_i].every(ch => ch =='0' || ch =='5')) {
-            result.push(Number(string_i));
+        if ([...String(i)].every(num => num =='0' || num =='5')) {
+            result.push(i);
         }
     }
     return result.length > 0 ? result : [-1]
