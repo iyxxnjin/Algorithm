46bda7353ed726ddd3152c94cb32fb14bc2d9a7f
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-06-21 13:27:53 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,12 +1,5 @@
 function solution(num_list) {
-    const last = num_list[num_list.length - 1]
-    const secondLast = num_list[num_list.length - 2]
-    
-    if (last > secondLast) {
-        num_list.push(last - secondLast)
-    }
-    else {
-        num_list.push(last * 2)
-    }
+    const [a, b] = [num_list[num_list.length-1], num_list[num_list.length-2]];
+    a > b ? num_list.push(a-b) : num_list.push(a*2);
     return num_list
 }
