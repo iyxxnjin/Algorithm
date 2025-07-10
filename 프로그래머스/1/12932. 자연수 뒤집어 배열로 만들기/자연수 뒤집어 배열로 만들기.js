6f2eda9a9c63b65a4acaa1c065285a819c1b1343
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-07-11 01:03:51 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,11 +1,3 @@
 function solution(n) {
-    let arr = [];
-    n_str = n.toString();
-    
-    for(let i=0; i<n_str.length; i++){
-        arr.push(Number(n_str[i]));
-    }
-    
-    return arr.reverse()
-
+    return (n + '').split('').reverse().map(n => parseInt(n))
 }
